405fbb37db613c2934d3811c0da71de736a0a4eb
aeolu3000 <ab34EF/(>
2021-02-13 18:14:35 +0100

    Add program loop

--- FirstRiderProjectConsole/Program.cs
+++ FirstRiderProjectConsole/Program.cs
@@ -6,7 +6,10 @@ namespace FirstRiderProjectConsole
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello World!");   
+            }
         }
     }
 }
